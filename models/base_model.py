f1dd9506d0aaa123fe0c3800d66c4a7806000641
Eltotino <totojaber1@gmail.com>
2021-05-17 14:56:27 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,11 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
         """ str representation"""
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                  self.__dict__))
+                                      self.__dict__))
 
     def save(self):
         """update ublic instance aatirbute 'updated_at'"""
