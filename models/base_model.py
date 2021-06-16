ea388e18af233f4870b47749e7c08d0d35c8a2cc
Eltotino <totojaber1@gmail.com>
2021-06-16 21:31:24 +0100

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         id (int) : public instance attribute
 
         """
-
+        from models import storage
         time_format = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
@@ -24,6 +24,8 @@ class BaseModel:
                     value = datetime.strptime(value, time_format)
                 if key != ("__class__"):
                     setattr(self, key, value)
+        else:
+            storage.new(self)
 
     def __str__(self):
         """ str representation"""
@@ -32,7 +34,9 @@ class BaseModel:
 
     def save(self):
         """update ublic instance aatirbute 'updated_at'"""
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns dictionnary containing all k/v of the instance"""
