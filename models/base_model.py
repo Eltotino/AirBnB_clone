8e072cbf993c025bdfce5b66200c310d3ca591a9
Eltotino <totojaber1@gmail.com>
2021-06-19 15:51:31 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,18 @@ from datetime import datetime
 
 class BaseModel():
     """BaseModel class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Class Constructor"""
         format_time = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ("created_at", "updated_at"):
+                    v = datetime.strptime(v, format_time)
+                if k != "__class__":
+                    setattr(self, k, v)
     def __str__(self):
         """ Str format"""
         return "[{}] ({}) ({})".format(self.__class__.__name__,
