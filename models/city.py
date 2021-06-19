fc0109fe3d7987b627ebf8fcc529d8557ee788df
Eltotino <totojaber1@gmail.com>
2021-06-19 20:09:13 +0100

    more classes

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class inherited from BaseModel class"""
+    state_id = ""
+    name = ""
