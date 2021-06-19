efa582025bf1b892afb192913b90faae90f629bb
Eltotino <totojaber1@gmail.com>
2021-06-19 16:26:21 +0100

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,6 @@ class FileStorage():
     __file_path = 'file.json'
     __objects = {}
 
-
     def all(self):
         """Returns a dictionary containing objects"""
         return FileStorage.__objects
