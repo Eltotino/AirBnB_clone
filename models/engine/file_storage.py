33fd7b72b0b840547a7fbc9eb8772d3be5168e66
Eltotino <totojaber1@gmail.com>
2021-06-19 20:53:32 +0100

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""[FileStorage class]
-"""
-from os import path
+"""FileStorage class"""
 import json
+from os import path
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -11,11 +10,9 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-
 class FileStorage():
-    """[FileStorage]
-    """
-    __file_path = "file.json"
+    """FileStorage"""
+    __file_path = "fichier.json"
     __objects = {}
 
     classes = {"BaseModel": BaseModel,
@@ -26,39 +23,31 @@ class FileStorage():
                "Review": Review}
 
     def all(self):
-        """[all]
-        Returns:
-            [dict]: [dictionnary containing objects]
-        """
+        """Returns a dictionary containing objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """[new]
-        Args:
-            obj ([object]): [object to be created]
-        """
+        """Sets the obj with obj to be created"""
         _id = obj.id
-        key = str(obj.__class__.__name__) + "." + _id
-        FileStorage.__objects[key] = obj
+        k = str(obj.__class__.__name__) + "." + _id
+        FileStorage.__objects[k] = obj
 
     def save(self):
-        """[save]
-        """
-        dct = {}
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            for k, v in FileStorage.__objects.items():
-                dct[k] = v.to_dict()
-            json.dump(dct, f, indent=4)
+        """serialises object to json file"""
+        dicto = {}
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
+            for cle, valeur in FileStorage.__objects.items():
+                dicto[cle] = valeur.to_dict()
+            json.dump(dicto, file)
 
     def reload(self):
-        """[reload]
-        """
+        """Deserialises json file to object"""
         if path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as f:
-                obj = json.load(f)
-                dct = {}
-                for k, v in obj.items():
-                    dct[k] = self.classes[v["__class__"]](**v)
-                FileStorage.__objects = dct
+            with open(FileStorage.__file_path, "r") as file:
+                obj = json.load(file)
+                dicto = {}
+                for cle, valeur in obj.items():
+                    dicto[cle] = self.classes[valeur["__class__"]](**valeur)
+                FileStorage.__objects = dicto
         else:
             return
